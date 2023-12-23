ee5b4f2c754d14b331abbeec1a55c83a96384d8b
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2023-12-23 15:16:10 +0100

    Add trips component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {TripsComponent} from "./trips/trips.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'trips', component: TripsComponent },
+];
