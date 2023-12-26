45bbf6a8145c5811dc18a72b67a99d2888955945
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2023-12-26 15:02:17 +0100

    Move currency selector to NavBar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {CartWidgetComponent} from "../cart-widget/cart-widget.component";
+import {CurrencySelectorComponent} from "../currency-selector/currency-selector.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     RouterLink,
-    CartWidgetComponent
+    CartWidgetComponent,
+    CurrencySelectorComponent
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
