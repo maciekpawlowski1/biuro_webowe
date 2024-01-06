add63bfdb12c60f6b11d1c8979231c97c7b2070d
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2024-01-06 11:42:39 +0100

    Start adding firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
 import {provideHttpClient, withFetch} from "@angular/common/http";
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
+import {firebaseConfig} from "./firebase-config";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch())]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch()),
+    importProvidersFrom(provideFirebaseApp(() =>
+        initializeApp(firebaseConfig)
+      )
+    ),
+    importProvidersFrom(provideFirestore(() => getFirestore()))
+  ]
 };
--- src/app/trips-data-provider.service.ts
+++ src/app/trips-data-provider.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {firstValueFrom} from 'rxjs';
+import {firstValueFrom, Observable} from 'rxjs';
 import {Trip} from "./Trip";
+import { Firestore, addDoc, collection, collectionData, deleteDoc, doc} from "@angular/fire/firestore";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TripsDataProvider {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private firestore: Firestore) {
+    this.tripsCollection = collection(this.firestore, 'trips');
+  }
 
   private apiUrl = 'http://localhost:3000/trips';
 
-  getTrips(): Promise<Trip[]> {
-    return firstValueFrom(this.http.get<Trip[]>(this.apiUrl))
+  private shouldTakeFromFirebase = true
+
+  private readonly tripsCollection;
+
+  async getTrips(): Promise<Trip[]> {
+    if(this.shouldTakeFromFirebase) {
+      return firstValueFrom(collectionData(this.tripsCollection, { idField: 'id' }) as Observable<Trip[]>);
+    } else {
+      return await firstValueFrom(this.http.get<Trip[]>(this.apiUrl))
+    }
   }
 
-  deleteTrip(tripId: string): Promise<void> {
-    return firstValueFrom(this.http.delete<void>(`${this.apiUrl}/${tripId}`))
+  async deleteTrip(tripId: string): Promise<void> {
+    if(this.shouldTakeFromFirebase) {
+      const tripDoc = doc(this.firestore, `trips/${tripId}`);
+      await deleteDoc(tripDoc);
+    }
+    else {
+      await firstValueFrom(this.http.delete<void>(`${this.apiUrl}/${tripId}`))
+    }
   }
 
-  addTrip(trip: Trip): Promise<void> {
-    return firstValueFrom(this.http.post<void>(this.apiUrl, trip))
+  async addTrip(trip: Trip): Promise<void> {
+    if(this.shouldTakeFromFirebase) {
+      await addDoc(this.tripsCollection, trip);
+    }
+    else {
+      await firstValueFrom(this.http.post<void>(this.apiUrl, trip))
+    }
   }
 }
